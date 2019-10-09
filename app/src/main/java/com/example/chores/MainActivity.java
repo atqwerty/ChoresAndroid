de00315318f72ca7373f7de8bab24ecea77df67b
Denis <dmarkitanov@gmail.com>
2019-10-09 10:31:52 +0600

    added currentUser

--- app/src/main/java/com/example/chores/MainActivity.java
+++ app/src/main/java/com/example/chores/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chores;
 
 import android.os.Bundle;
 
+import com.example.chores.classes.User;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -20,6 +21,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +31,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        User currentUser = new User("Denis", "Markitanov", "dmarkitanov@gmail.com",
+                "123");
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+//        LinearLayout navigationViewHeader = findViewById(R.id.nav_view_header);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -41,8 +49,16 @@ public class MainActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        View header = navigationView.getHeaderView(0);
+
+        TextView username = header.findViewById(R.id.username);
+        TextView email = header.findViewById(R.id.email);
+        username.setText(currentUser.getName() + " " + currentUser.getSurname().charAt(0));
+        email.setText(currentUser.getEmail());
+
+//      Passing each menu ID as a set of Ids because each
+//      menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_feed, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
