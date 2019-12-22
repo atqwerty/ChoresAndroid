736eadf6415c321cf3ecae18955c36d966419edf
Denis <dmarkitanov@gmail.com>
2019-12-22 20:19:37 +0600

    added button for the task status update request

--- app/src/main/java/com/example/chores/ativities/TaskActivity.java
+++ app/src/main/java/com/example/chores/ativities/TaskActivity.java
@@ -13,10 +13,15 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.chores.R;
 import com.example.chores.classes.Status;
 import com.example.chores.classes.Task;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -28,8 +33,9 @@ public class TaskActivity extends AppCompatActivity {
     private TextView description;
     private TextView info;
     private Button share;
+    private Button updateStatus;
     private Spinner spinner;
-    private static String[] options = {"asdf", "asdhfskadv"};
+    private int statusId, taskId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +48,12 @@ public class TaskActivity extends AppCompatActivity {
         info = this.findViewById(R.id.info);
         share = this.findViewById(R.id.buttonShare);
         spinner = this.findViewById(R.id.spinner);
+        updateStatus = this.findViewById(R.id.update_status);
 
         final Task a = (Task) getIntent().getSerializableExtra("targetTask");
+
+        taskId = a.getId();
+
         final ArrayList<Status> statuses = (ArrayList<Status>) getIntent().getSerializableExtra("statuses");
         ArrayList<String> statusNames = new ArrayList<>();
 
@@ -66,8 +76,8 @@ public class TaskActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.d("adsf", "onItemSelected: " + adapterView.getSelectedItem().toString());
                 if (!adapterView.getSelectedItem().toString().equals(a.getStatus())) {
-                    int statusId = getStatusId(a.getStatus(), statuses);
-                    int taskId = a.getId();
+                    statusId = getStatusId(a.getStatus(), statuses);
+                    updateStatus.setEnabled(true);
                 }
             }
 
@@ -96,6 +106,33 @@ public class TaskActivity extends AppCompatActivity {
                 shareIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
                 shareIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
                 startActivity(Intent.createChooser(shareIntent, "Share using"));
+            }
+        });
+
+        updateStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://chores-backend-atqwerty.herokuapp.com/board/" + a.getBoard().getId() + "/updateStatus";
+                JSONObject updateTaskStatusInfo = new JSONObject();
+                try {
+                    updateTaskStatusInfo.put("status_id", statusId);
+                    updateTaskStatusInfo.put("task_id", taskId);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, updateTaskStatusInfo,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                
+                            }
+                        }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                });
             }
         });
 
